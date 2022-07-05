1c9d12cccc9abc5e83b07ace81c42bb8edd5c886
Maciej Glapiak <maciej.glapiak@gotoma.pl>
2022-07-05 09:23:09 +0200

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,3 @@
-using API.Extensions;
-
 namespace API.Entities
 {
     public class AppUser
@@ -19,10 +17,8 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
-
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
